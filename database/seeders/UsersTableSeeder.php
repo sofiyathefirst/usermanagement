f4f224c513b68fe4755fe7c78b59745a1e77df15
sofiyathefirst <nursarahsofiyaomarbashah@gmail.com>
2023-10-19 22:15:54 +0800

    modified:   database/seeders/DatabaseSeeder.php 	modified:   database/seeders/UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert ([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make ('password')
+
+            ]
+            ]);
     }
 }
